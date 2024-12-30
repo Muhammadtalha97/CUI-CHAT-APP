df8a065f62bbe2e9be27d65a174c072e49026d75
Muhammad Talha <talhachoudhary50@gmail.com>
2024-12-30 09:38:25 +0500

    Update Home Page
    Home page have static Data
    Kindly click on Plus button for chat to anyone

--- lib/Pages/Home/Widgets/ChatLists.dart
+++ lib/Pages/Home/Widgets/ChatLists.dart
@@ -1,7 +1,6 @@
 import '/Config/Images.dart';
 import '/Pages/Home/Widgets/ChatTile.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class ChatLists extends StatelessWidget {
   const ChatLists({super.key});
@@ -10,57 +9,10 @@ class ChatLists extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        InkWell(
-          onTap: () {
-            Get.toNamed('/chatPage');
-          },
-          child: const ChatTile(
-            imageUrl: AssetsImage.girlPic,
-            name: 'Tooba Amjad',
-            lastChat: 'No More Time',
-            lastTime: '8:00 PM',
-          ),
-        ),
         const ChatTile(
           imageUrl: AssetsImage.boyPic,
           name: 'Talha CH',
-          lastChat: 'I Love You',
-          lastTime: '1:00 PM',
-        ),
-        const ChatTile(
-          imageUrl: AssetsImage.girlPic,
-          name: 'Tooba Amjad',
-          lastChat: 'No More Time',
-          lastTime: '8:00 PM',
-        ),
-        const ChatTile(
-          imageUrl: AssetsImage.boyPic,
-          name: 'Talha CH',
-          lastChat: 'I Love You',
-          lastTime: '1:00 PM',
-        ),
-        const ChatTile(
-          imageUrl: AssetsImage.girlPic,
-          name: 'Tooba Amjad',
-          lastChat: 'No More Time',
-          lastTime: '8:00 PM',
-        ),
-        const ChatTile(
-          imageUrl: AssetsImage.boyPic,
-          name: 'Talha CH',
-          lastChat: 'I Love You',
-          lastTime: '1:00 PM',
-        ),
-        const ChatTile(
-          imageUrl: AssetsImage.girlPic,
-          name: 'Tooba Amjad',
-          lastChat: 'No More Time',
-          lastTime: '8:00 PM',
-        ),
-        const ChatTile(
-          imageUrl: AssetsImage.boyPic,
-          name: 'Talha CH',
-          lastChat: 'I Love You',
+          lastChat: 'Static Files',
           lastTime: '1:00 PM',
         ),
       ],
